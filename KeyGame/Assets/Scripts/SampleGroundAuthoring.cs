5155dc5e4c810ac269ea79f4cc7272b4f7d9f38f
34-NAKAMURASOUTA <rsy50495@gmail.com>
2026-03-14 15:44:15 +0900

    サンプルシーンのオブジェクト変更

--- KeyGame/Assets/Scripts/SampleGroundAuthoring.cs
+++ KeyGame/Assets/Scripts/SampleGroundAuthoring.cs
@@ -1,36 +1,20 @@
 using UnityEngine;
 
-// サンプル用の地面に、見た目と当たり判定を付与する。
-[ExecuteAlways]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
 public sealed class SampleGroundAuthoring : MonoBehaviour
 {
     private const float GroundSpritePixelsPerUnit = 32f;
 
-    private void Awake()
+    private void Reset()
     {
-        EnsureComponents();
-    }
-
-    private void OnEnable()
-    {
-        EnsureComponents();
-    }
-
-    private void OnValidate()
-    {
-        EnsureComponents();
-    }
-
-    private void EnsureComponents()
-    {
-        // 地面は 仮の四角画像を使う。
         var renderer = GetOrAddComponent<SpriteRenderer>();
         renderer.sprite = SampleAssetLoader.LoadSprite("Common/PrototypeSquare.png", GroundSpritePixelsPerUnit);
         renderer.color = new Color(0.19f, 0.24f, 0.31f);
         renderer.sortingOrder = 0;
 
-        // 見た目と当たり判定の形を合わせるため、BoxCollider2D
-        GetOrAddComponent<BoxCollider2D>();
+        var collider2D = GetOrAddComponent<BoxCollider2D>();
+        collider2D.size = Vector2.one;
     }
 
     private T GetOrAddComponent<T>() where T : Component
