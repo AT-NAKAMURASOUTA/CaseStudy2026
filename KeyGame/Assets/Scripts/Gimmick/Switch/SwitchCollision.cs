50b25b49fc583146dada1bfd7237cd7fb289ffee
minto-131227 <146702006+minto-131227@users.noreply.github.com>
2026-03-31 12:20:59 +0900

    スイッチHit（押された）判定を返す

--- KeyGame/Assets/Scripts/Gimmick/Switch/SwitchCollision.cs
+++ KeyGame/Assets/Scripts/Gimmick/Switch/SwitchCollision.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class SwitchCollision : MonoBehaviour
 {
+    bool hitFlag = false;
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            Debug.Log("a");
+            hitFlag = true;
         }
+    }
+
+    //スイッチ押された判定を返す
+    public bool GetCollisionFlag()
+    {
+        return hitFlag;
     }
 }
