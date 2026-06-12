4309e87b3409614cefd53413a1eeab1468ddc6b7
MatsudaT <matsudat.30692@gmail.com>
2026-06-12 14:19:24 +0900

    シーン遷移時にズーム処理を作成

---
+++ KeyGame/Assets/Scripts/Action/1_Actions/Action_ZoomCamera.cs
@@ -0,0 +1,69 @@
+using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Action_ZoomCamera: BaseAction
+{
+    // ===========================================
+    // メンバー変数
+    // ===========================================
+    [Header("ズーム設定")]
+    [Tooltip("ズーム時間")]
+    [SerializeField] private float m_ZoomTime;
+    [Tooltip("ズーム補間率")]
+    [SerializeField, Range(0f, 1f)]
+    private float m_ApproachRate = 0.8f;
+
+    private Camera m_Camera; 
+    private Vector3 m_TargetPos;
+
+    // ===========================================
+    // 初期化
+    // ===========================================
+    public void Start()
+    {
+        // カメラの取得
+        m_Camera = Camera.main;
+
+        // カメラからターゲットのベクトルを計算
+        Vector3 toTarget = transform.position - m_Camera.transform.position;
+
+        // 最終位置
+        m_TargetPos = m_Camera.transform.position + toTarget * m_ApproachRate;
+    }
+
+    // ===========================================
+    // アクション実行
+    // ===========================================
+    public override async UniTask Execute(CancellationToken token)
+    {
+        // 開始状態を取得
+        Vector3 startPos = m_Camera.transform.position;
+
+        Debug.Log($"Zoom Start: Position={startPos}");
+        // 時間
+        float elapsed = 0f;
+
+        while (elapsed < m_ZoomTime)
+        {
+            token.ThrowIfCancellationRequested();
+
+            // 経過時間の更新
+            elapsed += Time.deltaTime;
+
+            // 0~1の範囲で補間値を計算
+            float t = Mathf.Clamp01(elapsed / m_ZoomTime);
+
+            // カメラのサイズと位置を補間して更新
+            m_Camera.transform.position =
+                Vector3.Lerp(startPos, m_TargetPos, t);
+
+            // 次のフレームまで待機
+            await UniTask.Yield(token);
+        }
+
+        // 最終的な状態を確実に設定
+        m_Camera.transform.position = m_TargetPos;
+    }
+}
